62f3b87288b4f8a873a1a14b73bfb582256bfd04
jhkim105 <jihwan.kim@rsupport.com>
2017-09-25 21:08:51 +0900

    jwt 적용

--- oauth2-server/src/main/java/com/example/demooauth2/oauth2server/AuthorizationServerConfig.java
+++ oauth2-server/src/main/java/com/example/demooauth2/oauth2server/AuthorizationServerConfig.java
@@ -1,14 +1,19 @@
 package com.example.demooauth2.oauth2server;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
-@Configuration
-@EnableAuthorizationServer
+import javax.sql.DataSource;
+
+//@Configuration
+//@EnableAuthorizationServer
 public class AuthorizationServerConfig {
 
-//	@Bean
-//  public TokenStore jdbcTokenStore(DataSource dataSource) {
-//	  return new JdbcTokenStore(dataSource);
-//  }
+	@Bean
+  public TokenStore jdbcTokenStore(DataSource dataSource) {
+	  return new JdbcTokenStore(dataSource);
+  }
+
+
 }
