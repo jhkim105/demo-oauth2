2077faa7c46852797cf4e0e480a23d2832193814
jhkim105 <just090323!>
2020-12-02 20:58:37 +0900

    get oauth token mvc test

--- auth-server/src/test/java/com/example/oauth2/OAuthMvcTest.java
+++ auth-server/src/test/java/com/example/oauth2/OAuthMvcTest.java
@@ -1,9 +1,13 @@
 package com.example.oauth2;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.UnsupportedEncodingException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
 import org.junit.jupiter.api.Disabled;
@@ -12,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.RequestPostProcessor;
@@ -19,32 +24,31 @@ import org.springframework.test.web.servlet.request.RequestPostProcessor;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @Slf4j
-//@Import(SecurityConfig.class)
 public class OAuthMvcTest {
 
   @Autowired
   MockMvc mockMvc;
 
   @Test
-  @Disabled("org.springframework.security.access.AccessDeniedException: Access is denied why?")
   void accessToken() throws Exception {
-    mockMvc.perform(
-      post("/oauth/token")
-        .header("Authorization", basic("client01", "secret01"))
-        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-        .param("grant_type", "client_credentials")
-        .param("scope", "USER")
-    )
-      .andDo(print())
-
+    String response = mockMvc.perform(
+        post("/oauth/token")
+            .header(HttpHeaders.AUTHORIZATION, basic("client01", "secret01"))
+            .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+            .param("grant_type", "client_credentials")
+            .param("scope", "foo"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andReturn().getResponse().getContentAsString();
     ;
-
+    assertThat(response).isNotBlank();
   }
 
   private String basic(String client01, String secret01) {
     String in = String.format("%s:%s", client01, secret01);
-    String out = new String(Base64.encodeBase64(in.getBytes()));
+    String out = Base64.encodeBase64String(in.getBytes());
     log.debug(out);
-    return out ;
+    return String.format("Basic %s", out) ;
   }
+
 }
